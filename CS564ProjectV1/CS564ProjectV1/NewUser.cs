981d8bf4a9cca3415b984446ee5a5344b0bf52f7
Thethoughtful1 <Lencho.dwight@gmail.com>
2016-12-04 14:55:30 -0600

    Use create user stored procedure

--- CS564ProjectV1/CS564ProjectV1/NewUser.cs
+++ CS564ProjectV1/CS564ProjectV1/NewUser.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,62 @@ namespace CS564ProjectV1
             this.Close();
             FrmLogin loginForm = new FrmLogin();
             loginForm.Show();
+        }
+
+        private void cmdRegister_Click(object sender, EventArgs e)
+        {
+            string login = txtUserName.Text;
+            string password = txtPassword.Text;
+            string firstName = txtFName.Text;
+            string lastName = txtFName.Text;
+
+            SqlCommand verifyLogin = new SqlCommand("CheckLogin", Main.connection);
+            verifyLogin.CommandType = CommandType.StoredProcedure;
+            verifyLogin.Parameters.AddWithValue("@login", login);
+            
+            bool loginVerified = ((int)verifyLogin.ExecuteScalar() == 0 ? false : true);
+
+            if (login == "" || login.Length > 50)
+            {
+                MessageBox.Show("Please enter a user name of up to 50 characters.");
+            }            
+            else if (password == "" || password.Length > 500)
+            {
+                MessageBox.Show("Please enter a password of up to 500 characters.");
+            }
+            else if (!loginVerified)
+            {
+                MessageBox.Show("Your login has been taken, please try another.");
+            }
+            else
+            {
+                SqlCommand cmd = new SqlCommand("CheckPassword", Main.connection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@login",txtUserName.Text);
+                cmd.Parameters.AddWithValue("@password", txtPassword.Text);
+
+            }
+            
+            SqlCommand verifyLogin = new SqlCommand("CheckPassword", Main.connection);
+            verifyLogin.CommandType = CommandType.StoredProcedure;
+            verifyLogin.Parameters.AddWithValue("@login",txtUserName.Text);
+            verifyLogin.Parameters.AddWithValue("@password", txtPassword.Text);
+
+            bool loginVerified = ((int)verifyLogin.ExecuteScalar() == 0 ? false : true);
+
+            if (loginVerified)
+            {
+                MessageBox.Show("Login Successful!");
+                this.Hide();
+                Main.login = txtUserName.Text;
+                UserProfile userProfile = new UserProfile();
+                userProfile.Show();
+            }
+            else
+            {
+                MessageBox.Show("User Name or Password is incorrect");
+            }
+        }
         }
     }
 }
