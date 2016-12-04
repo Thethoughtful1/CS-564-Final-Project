133413c6461afada27ae3e785431f730ea9a0c48
Thethoughtful1 <Lencho.dwight@gmail.com>
2016-12-04 14:29:52 -0600

    Universal connection

--- CS564ProjectV1/CS564ProjectV1/Main.cs
+++ CS564ProjectV1/CS564ProjectV1/Main.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,8 +13,12 @@ namespace CS564ProjectV1
 {
     public partial class Main : Form
     {
+        public static string login;
+        static string dbConnectionString = @"Server=Leap;Database=Locations;Integrated Security=true";
+        public static SqlConnection connection = new SqlConnection(dbConnectionString);
         public Main()
         {
+            connection.Open();
             InitializeComponent();
         }
 
